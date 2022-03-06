a4f1f70713766eb7c3dde9f7dc3482395d2d2f26
blalopsau <92573688+blalopsau@users.noreply.github.com>
2022-03-06 16:38:31 +0100

    creación de personaje y terreno

--- Assets/Scripts/ComprobarSuelo.cs
+++ Assets/Scripts/ComprobarSuelo.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class ComprobarSuelo : MonoBehaviour
 {
-   public static bool tocaSuelo;
+    public static bool tocaSuelo;
+    public static bool tocaLava;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -12,12 +13,22 @@ public class ComprobarSuelo : MonoBehaviour
         {
             tocaSuelo = true;
         }
+
+        if (collision.CompareTag("Lava"))
+        {
+            tocaLava = true;
+        }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Suelo"))
         {
             tocaSuelo = false;
+
+        }
+        if (collision.CompareTag("Lava"))
+        {
+            tocaLava = false;
 
         }
     }
--- Assets/Scripts/Controlador.cs
+++ Assets/Scripts/Controlador.cs
@@ -19,40 +19,49 @@ public class Controlador : MonoBehaviour
         TipoAnimacion tipoAnimacion = TipoAnimacion.Parado;
 
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        Vector2 posicion = transform.position;
 
+        int vidas = 10;
         // dirX toma valores entre -1 y 1
 
         if (Input.GetKey("a"))
         {
             tipoAnimacion = TipoAnimacion.Correr;
-            rb.velocity = new Vector2(-8, rb.velocity.y);
+            rb.velocity = new Vector2(-7, rb.velocity.y);
             spRender.flipX = true;
         }
         else if (Input.GetKey("d"))
         {
             tipoAnimacion = TipoAnimacion.Correr;
-            rb.velocity = new Vector2(8, rb.velocity.y);
+            rb.velocity = new Vector2(7, rb.velocity.y);
             spRender.flipX = false;
-        } else {
-			rb.velocity = new Vector2(0, rb.velocity.y);
-			tipoAnimacion = TipoAnimacion.Parado;
-		}
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+            tipoAnimacion = TipoAnimacion.Parado;
+        }
 
-		if (Input.GetKey(KeyCode.Space) && ComprobarSuelo.tocaSuelo) {
-            rb.AddForce(new Vector2(0, 100));
-		}
+        if (Input.GetKey(KeyCode.Space) && ComprobarSuelo.tocaSuelo)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 7);
+        }
 
         if (Mathf.Abs(rb.velocity.y) > 0.1f)
         {
             tipoAnimacion = TipoAnimacion.Saltar;
 
         }
+        if (ComprobarSuelo.tocaLava)
+        {
+            vidas -= 1;
+        }
 
 
         animacion.SetInteger("estado", (int)tipoAnimacion);
     }
 
-     public enum TipoAnimacion
+    public enum TipoAnimacion
     {
         Parado,
         Correr,
